dd968a0a377e369c656668d34e5b3b8391985b4e
danilmv <43599357+danilmv@users.noreply.github.com>
2021-05-23 16:43:44 +0300

    L3: calculator: button action

---
+++ calculator/src/main/java/com/andriod/calculator/Action.java
@@ -0,0 +1,34 @@
+package com.andriod.calculator;
+
+import java.io.Serializable;
+
+public enum Action implements Serializable {
+    ZERO("0"), ONE("1"), TWO("2"), THREE("3"), FOUR("4"),
+    FIVE("5"), SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"),
+    DECIMAL("."),
+    CANCEL("C", true),
+    SIGN("+/-", true),
+    PERCENT("%", true), DIVIDE("/", true),
+    MULTIPLY("*", true), SUBTRACT("-", true),
+    ADD("+", true), EQUAL("=", true);
+
+    private final String value;
+    private final boolean isOperator;
+
+    Action(String value, boolean isOperator) {
+        this.value = value;
+        this.isOperator = isOperator;
+    }
+    Action(String value) {
+        this.value = value;
+        this.isOperator = false;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isOperator(){
+        return isOperator;
+    }
+}
