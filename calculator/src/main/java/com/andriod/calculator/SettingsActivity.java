86c3cd837caace2ed99b25403b88c59f183ace92
danilmv <43599357+danilmv@users.noreply.github.com>
2021-05-28 17:38:31 +0300

    L5: intents: SettingsActivity: clickListener

--- calculator/src/main/java/com/andriod/calculator/SettingsActivity.java
+++ calculator/src/main/java/com/andriod/calculator/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.andriod.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ToggleButton toggleButtonTheme = findViewById(R.id.toggle_button_theme_switch);
+        toggleButtonTheme.setChecked((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES);
+        toggleButtonTheme.setOnCheckedChangeListener(
+                (buttonView, isChecked) -> AppCompatDelegate.setDefaultNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO));
     }
 }
